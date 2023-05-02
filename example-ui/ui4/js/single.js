0d2b1b75ebf8153414eba1815866cd90b35f56cb
Sami-Rasouli <sami.rasouli@icloud.com>
2023-05-02 11:12:01 +0300

    single

--- example-ui/ui4/js/single.js
+++ example-ui/ui4/js/single.js
@@ -3,9 +3,9 @@ import {url} from '../../conf.js';
 
 // get query parameter
 const getQParam = (param) => {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  return urlParams.get(param);
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get(param);
 };
 
 // get id from address
@@ -16,15 +16,15 @@ const img = document.querySelector('#image img');
 
 // add existing cat data to form
 const getCat = async (id) => {
-  const fetchOptions = {
-    headers: {
-      Authorization: 'Bearer ' + sessionStorage.getItem('token'),
-    },
-  };
-  const response = await fetch(url + '/cat/' + id, fetchOptions);
-  const cat = await response.json();
-  img.src = `${url}/${cat.filename}`;
-  addMarker(JSON.parse(cat.coords));
+    const fetchOptions = {
+        headers: {
+            Authorization: 'Bearer ' + sessionStorage.getItem('token')
+        }
+    };
+    const response = await fetch(url + '/cat/' + id, fetchOptions);
+    const cat = await response.json();
+    img.src = `${url}/${cat.filename}`;
+    addMarker(JSON.parse(cat.coords));
 };
 
 getCat(cat_id);
